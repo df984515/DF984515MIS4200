99e8de1b0988ff7ab377aa712b4212f94f3fd1bd
Daniel Foster <df984515@ohio.edu>
2020-02-07 11:04:21 -0500

    2/7/2020 Class

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace DF984515MIS4200.Models
     public class Customer
     {
         public int customerID { get; set; }
+        [Display(Name = "First Name")]
         public string firstName { get; set; }
+        [Display(Name = "Last Name")]
         public string lastName { get; set; }
+        [Display(Name = "Email")]
         public string email { get; set; }
+        [Display(Name = "Phone Number")]
         public string phone { get; set; }
+        [Display(Name = "Customer Since")]
         public DateTime customerSince { get; set; }
 
         public ICollection<Orders> Orders{ get; set; }
