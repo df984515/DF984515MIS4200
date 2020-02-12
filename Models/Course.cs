0fda4ee527d45f2cc37a49210882d33697bb11e3
Daniel Foster <df984515@ohio.edu>
2020-02-12 10:56:12 -0500

    SD2 Class 2/10/2020

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -10,11 +12,19 @@ namespace DF984515MIS4200.Models
     {
         [Key]
         public int courseID { get; set; }
-        public string description { get; set; }
+        [Display(Name = "Course Name")]
+        public string courseName { get; set; }
+        [Display(Name = "Start Date")]
         public DateTime startDate { get; set; }
+        [Display(Name = "End Date")]
         public DateTime endDate { get; set; }
 
-        public int studentID { get; set; }
-        public virtual Student Student { get; set; }
+        public int instructorID { get; set; }
+        public virtual Instructor Instructor { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
     }
 }
---
+++ Models/Instructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace DF984515MIS4200.Models
+{
+    public class Instructor
+    {
+        [Key]
+        public int instructorID { get; set; }
+        [Display (Name ="Full Name")]
+        public string fullName { get { return lastName + ", " + firstName; } }
+        [Display (Name = "First Name")]
+        public string firstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Display(Name = "Email Address")]
+        public string email { get; set; }
+        [Display(Name = "Phone Number")]
+        public string phoneNumber { get; set; }
+
+        public ICollection<Course> Course { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+    }
+}
